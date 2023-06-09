1be47bb8a9982fc7e61052e09cc3c0b4f5d976b5
Vishwajeet Roy <vishwajeetroy@Vishwajeets-MacBook-Pro.local>
2023-06-09 15:01:54 -0400

    pushing terraform code

---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+    region = "us-east-1"
+    profile = "terraform-user"
+}
+
+module "vpc" {
+    source = "terraform-aws-modules/vpc/aws"
+    version = "3.14.0"
+
+    name = var.vpc_name
+    cidr = var.vpc_cidr
+
+    azs = var.vpc_azs
+    private_subnets = var.vpc_private_subnets
+    public_subnets = var.vpc_public_subnets
+
+    enable_nat_gateway = var.vpc_enable_nat_gateway
+
+    tags = var.vpc_tags
+}
+
+module "ec2_instances" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "3.5.0"
+  
+  for_each = var.instances
+  ami                    = var.ami
+  instance_type           = each.value.instance_type
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  tags = {
+    Name        = "${each.key}.${var.ins_name}"
+    Terraform   = "true"
+    Environment = var.env_name
+  }
+}
+
+resource "aws_ebs_volume" "partition" {
+      for_each          = var.volumes
+      availability_zone = each.value.availability_zone
+      size              = each.value.size
+    }
+
+resource "aws_volume_attachment" "ebs_att" {
+  for_each    = aws_ebs_volume.partition
+  device_name = contains(["Primary", "Worker1", "Worker2"], each.key) ? "/dev/sdf" : "/dev/sdg"
+  volume_id   = each.value.id
+  instance_id = module.instances[each.value.availability_zone].id
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,90 @@
 variable "region" {
-default = "us-east-1"
+  default = "us-east-1"
+}
+
+variable "vpc_name" {
+  default = "demo"
+  type = string
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+  type = string
+}
+
+variable "vpc_azs" {
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  type        = list(string)
+}
+
+variable "vpc_private_subnets" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  type        = list(string)
+}
+
+variable "vpc_public_subnets" {
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+  type        = list(string)
+}
+
+variable "vpc_enable_nat_gateway" {
+  default = true
+  type        = bool
+}
+
+variable "vpc_tags" {
+  default = {
+    Terraform   = "true"
+    Environment = "development"
+  }
+}
+
+variable "instances" {
+  type = map
+  default     = {
+    testing-dev = {
+      instance_count          = 1,
+      instance_type           = "t3.micro",
+      environment             = "dev"
+    },
+    testing-stage = {
+      instance_count          = 1,
+      instance_type           = "t3.micro",
+      environment             = "test"
+    }
+  }
+}
+
+variable "ami" {
+  default = "ami-0015a39e4b7c0966f"
+  type        = string
+}
+
+variable "ins_name" {
+  default = "demo"
+  type = string
+}
+variable "env_name" {
+  default             = "dev"
+}
+
+variable "volumes" {
+  type = map(object({
+    size              = string
+    availability_zone = string
+  }))
+  default = {
+    "Primary" = {
+      size              = "10"
+      availability_zone = "us-east-2a"
+    }
+    "Worker1" = {
+      size              = "200"
+      availability_zone = "us-east-2b"
+    }
+    "Worker2" = {
+      size              = "200"
+      availability_zone = "us-east-2c"
+    }
+  }
 }
